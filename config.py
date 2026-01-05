f03015db90197b0b48e63171be4e26f31c6e751c
ixqoo <ixqoo812@gmail.com>
2026-01-06 01:10:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+ADMIN_IDS = [123456789, 987654321]
+
+VPN_LINK = "https://example.com/vpn"
+
+PLAYLIST_LINKS = {
+    "comfort": "https://example.com/comfort",
+    "games": "https://example.com/games",
+    "random": "https://example.com/random"
+}
